d44f83a0ef991bf2d79c883c543be5d08703efec
nazliasrg <alviranazli@gmail.com>
2021-03-26 09:58:09 +0700

    Perbaikan Card

--- src/components/User/GenereCard/index.js
+++ src/components/User/GenereCard/index.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import './GenreCard.css'
 import Novel from '../../../assets/book/B001.jpg';
 import Comic from '../../../assets/book/B005.jpg';
@@ -9,40 +9,40 @@ import Personal from '../../../assets/book/B004.jpg';
 import Computer from '../../../assets/book/B002.jpg';
 import Business from '../../../assets/book/B017.jpg';
 
-const GenreCard= () =>{
-    return(
+const GenreCard = () => {
+    return (
         <Fragment>
-            <div className="container">
-                <div className="row d-flex justify-content-center col-md-12" style={{height: '60px'}}>
+            <div className="containerGenre">
+                <div className="row d-flex justify-content-center col-md-12" style={{ height: '60px' }}>
                     <h4 className="title">B&nbsp;O&nbsp;O&nbsp;K&nbsp;S&nbsp; G&nbsp;E&nbsp;N&nbsp;R&nbsp;E&nbsp;</h4>
                 </div>
                 <div className="row">
                     <div className="d-flex justify-content-center col-12">
-                        <a href="#" className="col-2.5"><div className="card border-0 mx-3">
-                            <img className="genre-pic" src={Novel} alt=""/>
-                            <div class="card-body">
-                                <hr/>
+                        <a href="#" className="col-2.5"><div className="cardGenre border-0 mx-3">
+                            <img className="genre-pic" src={Novel} alt="" />
+                            <div class="card-body-genre">
+                                <hr />
                                 <h5>Novel</h5>
                             </div>
                         </div></a>
-                        <a href="#" className="col-2.5"><div className="card border-0 mx-3">
-                            <img className="genre-pic" src={Comic} alt=""/>
-                            <div class="card-body">
-                                <hr/>
+                        <a href="#" className="col-2.5"><div className="cardGenre border-0 mx-3">
+                            <img className="genre-pic" src={Comic} alt="" />
+                            <div class="card-body-genre">
+                                <hr />
                                 <h5>Comic Fantasy</h5>
                             </div>
                         </div></a>
-                        <a href="#"><div className="card border-0 mx-3">
-                            <img className="genre-pic" src={Romance} alt=""/>
-                            <div class="card-body">
-                                <hr/>
+                        <a href="#"><div className="cardGenre border-0 mx-3">
+                            <img className="genre-pic" src={Romance} alt="" />
+                            <div class="card-body-genre">
+                                <hr />
                                 <h5>Romance</h5>
                             </div>
                         </div></a>
-                        <a href="#"><div className="card border-0 mx-3">
-                            <img className="genre-pic" src={Travel} alt=""/>
-                            <div class="card-body">
-                                <hr/>
+                        <a href="#"><div className="cardGenre border-0 mx-3">
+                            <img className="genre-pic" src={Travel} alt="" />
+                            <div class="card-body-genre">
+                                <hr />
                                 <h5>Travel</h5>
                             </div>
                         </div></a>
@@ -50,31 +50,31 @@ const GenreCard= () =>{
                 </div>
                 <div className="row border-0 mt-4">
                     <div className="d-flex flex-row justify-content-center col-md-12">
-                        <a href="#"><div className="card border-0 mx-3">
-                            <img className="genre-pic" src={History} alt=""/>
-                            <div class="card-body">
-                                <hr/>
+                        <a href="#"><div className="cardGenre border-0 mx-3">
+                            <img className="genre-pic" src={History} alt="" />
+                            <div class="card-body-genre">
+                                <hr />
                                 <h5>History</h5>
                             </div>
                         </div></a>
-                        <a href="#"><div className="card border-0 mx-3">
-                            <img className="genre-pic" src={Personal} alt=""/>
-                            <div class="card-body">
-                                <hr/>
+                        <a href="#"><div className="cardGenre border-0 mx-3">
+                            <img className="genre-pic" src={Personal} alt="" />
+                            <div class="card-body-genre">
+                                <hr />
                                 <h5>Personal Development</h5>
                             </div>
                         </div></a>
-                        <a href="#"><div className="card border-0 mx-3">
-                            <img className="genre-pic" src={Computer} alt=""/>
-                            <div class="card-body">
-                                <hr/>
+                        <a href="#"><div className="cardGenre border-0 mx-3">
+                            <img className="genre-pic" src={Computer} alt="" />
+                            <div class="card-body-genre">
+                                <hr />
                                 <h5>Computer & Tech</h5>
                             </div>
                         </div></a>
-                        <a href="#"><div className="card border-0 mx-3">
-                            <img className="genre-pic" src={Business} alt=""/>
-                            <div class="card-body">
-                                <hr/>
+                        <a href="#"><div className="cardGenre border-0 mx-3">
+                            <img className="genre-pic" src={Business} alt="" />
+                            <div class="card-body-genre">
+                                <hr />
                                 <h5>Business</h5>
                             </div>
                         </div></a>
